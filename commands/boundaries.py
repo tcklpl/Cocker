b117230d7c3a223f45e46b8257a438ab83e12936
tcklpl <luan.negroni@aluno.ifsp.edu.br>
2022-08-05 12:54:45 -0300

    input utils now only receives the dice string

--- commands/boundaries.py
+++ commands/boundaries.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord.ext.commands.cog import Cog
+from discord.ext.commands import Context
 from utils import input_utils
 
 class Boundaries(Cog):
@@ -8,14 +9,14 @@ class Boundaries(Cog):
         self.bot = bot
 
     @commands.command(name = 'boundaries', aliases = ['b'])
-    async def boundaries(self, ctx):
+    async def boundaries(self, ctx: Context):
 
         if (input_utils.check_if_will_execute()):
             await ctx.channel.send(ctx.author.mention + " Vai se foder")
             return
 
         try:
-            dices, simple_values = input_utils.split_raw_input_into_dices(ctx.message.content)
+            dices, simple_values = input_utils.split_input_into_dices(''.join(ctx.message.content.split()[1:]))
             min, max = self.get_roll_boundaries(dices, simple_values)
 
             await ctx.channel.send(ctx.author.mention + " Você pode rolar isso entre {} e {}".format(str(min), str(max)))
--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,15 +1,16 @@
 import re, random
+from typing import List
 from dice import Dice
 
-def split_raw_input_into_dices(raw_input):
-    input_without_command = ("".join(raw_input.split( )[1:])).replace(" ", "").lower()
+def split_input_into_dices(input: str):
+    input_without_command = input.replace(" ", "").lower()
     input_splitted_into_dices_and_numbers = re.findall('([+-]{0,1}[0-9]*d[0-9]+)|([+-]{0,1}[0-9]+)', input_without_command)
 
     if len(input_splitted_into_dices_and_numbers) == 0:
         raise ValueError("Input mal formatada")
 
-    dices = []
-    simple_values = []
+    dices: List[Dice] = []
+    simple_values: List[int] = []
 
     for arg in input_splitted_into_dices_and_numbers:
         possible_dice   = arg[0]
@@ -36,4 +37,4 @@ def split_raw_input_into_dices(raw_input):
     return dices, simple_values
 
 def check_if_will_execute() -> bool:
-    return random.randint(1, 100) <= 5
+    return random.randint(1, 1000) <= 1
